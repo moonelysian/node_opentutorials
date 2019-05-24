2cde1aff13e364035e042fda4cad6e934724b33f
moonelysian <dandyblack@naver.com>
2019-05-24 14:50:39 +0900

    make todolist with nodejs

---
+++ express/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var router = require('./routes/router')(app);
+
+app.set('views', __dirname + '/views');
+app.set('view engine','ejs');
+app.engine('html', require('ejs').renderFile);
+
+var server = app.listen(3000, function(){
+    console.log("Express server has started");
+});
+
+app.use(express.static('public'));
