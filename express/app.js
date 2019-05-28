87c7346abb0fac9de4a1d8f351afa7d737d5795a
moonelysian <dandyblack@naver.com>
2019-05-28 18:02:07 +0900

    mysql 연동

--- express/app.js
+++ express/app.js
@@ -1,13 +1,37 @@
 var express = require('express');
 var app = express();
-var router = require('./routes/router')(app);
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var dbconfig = require('./config/database.js');
+var connection = mysql.createConnection(dbconfig);
 
-app.set('views', __dirname + '/views');
+
 app.set('view engine','ejs');
-app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.static('public'));
 
-var server = app.listen(3000, function(){
+var todoList=[];
+
+// routes
+app.get('/',function(req,res){
+   connection.query(`select * from todos`, function(err, todo){
+        console.log(todo);
+    });
+    res.render('index.ejs', {todoList: todoList});
+});
+
+app.post('/create', function(req, res){
+    console.log('create!!');
+    var item = req.body.item;
+    todoList.push(item);
+    res.redirect('/');
+});
+
+app.get('*', function(req,res){
+    res.send(`<h1>Invalid Page</h1>`)
+})
+
+app.listen(3000, function(){
     console.log("Express server has started");
 });
 
-app.use(express.static('public'));
