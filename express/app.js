bb9c424fc71727f852e35ad7990e1c758076c013
moonelysian <dandyblack@naver.com>
2019-06-05 20:24:28 +0900

    delete 기능 구현 delete 메소드를 지원하지 않는다는걸 알았다 delete를 사용하려면 별도의 패키지 필요

--- express/app.js
+++ express/app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var qs = require('querystring');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var dbconfig = require('./config/database.js');
@@ -16,15 +15,12 @@ var todoList=[];
 // routes
 app.get('/',function(req,res){
    connection.query(`SELECT * FROM todos`, function(err, todo){
-        console.log(todo);
         res.render('index.ejs', {todoList: todo});
     });
-    //res.render('index.ejs', {todoList: todoList});
 });
 
 app.post('/create', function(req, res){
     var item = req.body.item;
-    console.log(item);
     connection.query(`INSERT INTO todos (content) VALUES ("${item}")`, function(err){
         try{
             res.redirect('/');
@@ -35,9 +31,20 @@ app.post('/create', function(req, res){
     });
 });
 
+app.post('/delete/:id', function(req, res){
+    var id = req.params.id;
+    connection.query(`DELETE FROM todos WHERE id=?`, id, function(err){
+        try{
+            res.redirect('/');
+        }catch(err){
+            throw err;
+        }
+    })
+});
+
 app.get('*', function(req,res){
     res.send(`<h1>Invalid Page</h1>`)
-})
+});
 
 app.listen(3000, function(){
     console.log("Express server has started");
